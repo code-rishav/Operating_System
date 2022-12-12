a801b5dc7bd21b7a8d4f8114e0003c48b06ed1e4
code-rishav <raj08rishav@gmail.com>
2022-12-13 03:24:16 +0530

    updated fork1

--- operating_system/fork1.cpp
+++ operating_system/fork1.cpp
@@ -5,15 +5,6 @@ int main(){
     int pid;
     cout<<"Execution before fork"<<endl;
     pid = fork();
-    if(pid>0){
-         cout<<"Parent execution"<<endl;
-         wait(NULL);
-    }
-    else{
-        cout<<"Child execution"<<endl;
-        for(long long i=0;i<1000000;i++);
-    }
-    
     cout<<"Execution after fork"<<endl;
     cout<<pid<<endl;
 }
